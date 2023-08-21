f953015c2dc2fdcfb877acc5432f92d896b028c3
Ali Sobhani <acdota@outlook.com>
2023-08-22 00:07:54 +0330

    Using Migration and EF Core to update database models

---
+++ DataService/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Entities.DbSet;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Define db entities
+        public virtual DbSet<Driver> Drivers { get; set; }
+        public virtual DbSet<Achievement> Achievements { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // define relations between entities
+
+            modelBuilder.Entity<Achievement>(entity =>
+                entity.HasOne(d => d.Driver)
+                .WithMany(a => a.Achievements)
+                .HasForeignKey(di => di.DriverId)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Achievements_Driver"));
+        }
+
+    }
+}
--- FormulaOne/Program.cs
+++ FormulaOne/Program.cs
@@ -1,6 +1,14 @@
+using DataService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// add dependency injection
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
