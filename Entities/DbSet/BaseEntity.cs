779bd6a0be32f6ffd1ecb986fe9527bbb1b48554
Ali Sobhani <acdota@outlook.com>
2023-08-21 23:21:53 +0330

    Entities added to project

---
+++ Entities/DbSet/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DbSet
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; } = new Guid();
+        public DateTime AddedDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ Entities/DbSet/Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DbSet
+{
+    public class Driver : BaseEntity
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int DriverNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Driver()
+        {
+            Achievements = new HashSet<Achievement>();
+        }
+
+        public virtual ICollection<Achievement> Achievements { get; set;}
+
+    }
+}
